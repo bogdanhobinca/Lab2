e65d3d7d973458989c8e80016a41e1c6d27f0755
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 22:18:59 +0000

    Modified

--- addHero.php
+++ addHero.php
@@ -6,7 +6,24 @@
     $flying=$_POST["Flying"];
     $spittingFire=$_POST["SpittingFire"];
 
-    echo $forename." ".$surname." born in ".$dob."\r\n"."Super Powers:"."\r\n"
-            ."Super Speed ".$superSpeed."\r\n"."Flying ".$flying."\r\n"."Spitting Fire ".$spittingFire;
+    $rez=$forename." ".$surname." born in ".$dob."\r\n"."Super Powers:"."\r\n";
+
+
+    if ($superSpeed!=null){
+        $rez=$rez."Super Speed true"."\r\n";
+    }else{
+        $rez=$rez."Super Speed false"."\r\n";
+    }
+    if ($flying!=null){
+        $rez=$rez."Flying true"."\r\n";
+    }else {
+        $rez=$rez."Flying false"."\r\n";
+    }
+    if ($spittingFire!=null){
+        $rez=$rez."Spitting Fire true";
+    }else{
+        $rez=$rez."Spitting Fire false";
+    }
+
 
 ?>
