3e079ceecb3d8151318607a72e92694ec8b948b5
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 22:11:21 +0000

    Modified

---
+++ addHero.php
@@ -0,0 +1,11 @@
+<?php
+    $forename=$_POST["forename"];
+    $surname=$_POST["surname"];
+    $dob=$_POST["dob"];
+    $superSpeed=$_POST["SuperSpeed"];
+    $flying=$_POST["Flying"];
+    $spittingFire=$_POST["SpittingFire"];
+
+    echo $forename." ".$surname." born in ".$dob."\r\n"."Super Powers:"."\r\n";
+
+?>
