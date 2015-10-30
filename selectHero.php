f09401c0ec5d8b59cbb57b45e8ce2f89fe879111
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 23:47:56 +0000

    Renamed

---
+++ selectHero.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head></head>
+<body>
+<form action="heroes.php" method="POST">
+    <div style="display:block;">
+        <label>Select from dropdown</label><br/>
+        <select name="val">
+            <option value="before2010">Before 2010</option>
+            <option value="marvel">Marvel Studio</option>
+            <option value="after2010">After 2010</option>
+            <option value="oscar">Oscar</option>
+        </select>
+    </div>
+    <br />
+    <input type="submit" value="submit" />
+</form>
+
+</body>
+</html>
