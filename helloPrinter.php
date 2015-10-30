ff3bf958dc37d3cf8040fb047a30a599e88affb6
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 20:46:14 +0000

    Added new page. Modified 'helloSelector'.

---
+++ helloPrinter.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head></head>
+<body>
+<?php
+    $query=$_GET["plane"];
+    echo "Hello".$query;
+?>
+</body>
+</html>
--- helloSelector.php
+++ helloSelector.php
@@ -3,11 +3,11 @@
 <head></head>
 <body>
 <p>Where would you like to say hello?</p>
-<br />
+<br /><br />
 <a href="http://lab2test.azurewebsites.net/helloPrinter.php?planet=earth">Earth</a>
-<br />
-<a href="http://lab2test.azurewebsites.net/helloPrinter.php?planet=Mars">Earth</a>
-<br />
-<a href="http://lab2test.azurewebsites.net/helloPrinter.php?planet=Uranus">Mars</a>
+<br /><br />
+<a href="http://lab2test.azurewebsites.net/helloPrinter.php?planet=Mars">Mars</a>
+<br /><br />
+<a href="http://lab2test.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a>
 </body>
 </html>
