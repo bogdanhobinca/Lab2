fa9217a2ebd5acbf93eba51ba771dc60fc20aec9
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 23:57:39 +0000

    Renamed

--- heroes.php
+++ heroes.php
@@ -5,7 +5,6 @@
 <?php
 
     $val=$_POST["val"];
-    echo $val;
 
     $db=new mysqli("br-cdbr-azure-south-a.cloudapp.net", "beb61afec52c3d", "62ec6fa4", "superHeroDatabase");
     if ($db->connect_errno){
@@ -13,6 +12,14 @@
     }
 
     $sql_query="Select * from marvelmovies";
+    switch($val){
+        case "before2010": $sql_query=$sql_query." where yearReleased < 2010"; break;
+        case "marvel": $sql_query=$sql_query." where productionStudio='Marvel Studios'"; break;
+        case "after2010": $sql_query=$sql_query." where yearReleased < 2010"; break;
+        case "oscar": $sql_query=$sql_query." where notes like '%oscar%'"; break;
+        default: break;
+    }
+
     $result=$db->query($sql_query);
 
     while ($row=$result->fetch_array()){
