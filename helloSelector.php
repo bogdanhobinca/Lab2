e5a1b8c16d3748db623fa519a8712a3944b12f56
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-30 20:36:52 +0000

    Initial commit

---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head></head>
+<body>
+<p>Where would you like to say hello?</p>
+<br />
+<a href="http://www.mysite.com/helloPrinter.php?planet=earth">Earth</a>
+<br />
+<a href="http://www.mysite.com/helloPrinter.php?planet=Mars">Earth</a>
+<br />
+<a href="http://www.mysite.com/helloPrinter.php?planet=Uranus">Mars</a>
+</body>
+</html>
